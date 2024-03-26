ce9574c71e4b51418c9270a7235e1fd14614b78c
mkjl <57776548+mkjlong@users.noreply.github.com>
2024-03-26 09:08:37 -0400

    first commit

--- src/virtualpet/VirtualPet.java
+++ src/virtualpet/VirtualPet.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package virtualpet;
-import java.util.*;
+import java.util.Scanner;
 /**
  *
  * @author michael.roy-diclemen
@@ -14,7 +14,85 @@ public class VirtualPet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        final String REQUIRED_USERNAME = "snoopy";
+        final String REQUIRED_PASSWORD = "toto";
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        // --- PART 1: MENU --- //
+        
+        System.out.println("------------------------------------------");
+        System.out.println("|               \\`*-.                    |");
+        System.out.println("|                )  _`-.                 |");
+        System.out.println("|               .  : `. .                |");
+        System.out.println("|               : _   '  \\               |");
+        System.out.println("|               ; o` _.   `*-._          |");
+        System.out.println("|      PET      `-.-'          `-.       |");
+        System.out.println("|   SIMULATOR     ;       `       `.     |");
+        System.out.println("| -------------   :.       .        \\    |");
+        System.out.println("| 1) START        . \\  .   :   .-'   .   |");
+        System.out.println("| 2) INSTRUCTIONS '  `+.;  ;  '      :   |");
+        System.out.println("| 3) EXIT         :  '  |    ;       ;-. |");
+        System.out.println("|                 ; '   : :`-:     _.`* ;|");
+        System.out.println("|                /  .*' ; .*`- +'  `*'   |");
+        System.out.println("|               *-*   `*-*  `*-*'        |");
+        System.out.println("------------------------------------------");
+        
+        System.out.print("|");
+        String option = keyboard.nextLine().toLowerCase();
+        
+        switch (option){
+            case "3":
+            case "exit":
+                System.out.println("Goodbye!!");
+                System.exit(0);
+            case "1":
+            case "start":
+                System.out.println("------------------------------------------");
+                System.out.println("|        Welcome to pet simulator!       |");
+                System.out.println("|          Choose a starter pet          |");
+                System.out.println("| -------------------------------------- |");
+                System.out.println("|  1)DOG     2)CAT    3)DUCK   4)FISH    |");
+                System.out.println("------------------------------------------");
+                
+                String petOption = keyboard.nextLine().toLowerCase();
+                
+                String petName = "";
+                
+                switch(petOption){
+                    case "1":
+                    case "dog":
+                        petName="dog";
+                        break;
+                    case "2":
+                    case "cat":
+                        petName="car";
+                        break;
+                    case "3":
+                    case "duck":
+                        petName="duck";
+                        break;
+                    case "4":
+                    case "fish":
+                        petName="fish";
+                        break;
+                }
+                System.out.println("You have successfully chosen a " + petName + " as your pet.");
+                
+                
+                
+                        
+                break;
+            case "2":
+            case "instructions":
+                System.out.println("INSTRUCTIONS");
+                break;
+            default:
+                System.out.println("Please enter a valid choice!");
+                break;
+        }
+        
     }
     
 }
